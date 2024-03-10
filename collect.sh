9e7baed911a6205019ba6c0a0777ec5110e6b331
opensourcer2 <0x54e2c404c4C9Bf7722d4AC4f242F64C9be36fadd@hashmail.dev>
2024-03-10 11:36:27 -0700

    Update main.yml and collect.sh

--- collect.sh
+++ collect.sh
@@ -10,25 +10,34 @@ popd
 
 tags=$(cat tag.txt)
 for tag in $tags; do
-    version=$(echo ${tag} | sed -e "s/^\s*./V/g" | sed -e "s/-/_/g" | sed -e "s/\./_/g")
-    echo "Processing version: ${version}"
-    pushd CoppeliaSimLib && tag_date=$(git log -1 --format=%as coppeliasim-${tag}) && popd
-    year=$(date -d ${tag_date} +%y)
-    nums=(0 2 4)
-    for i in ${nums[@]}; do
-        if ((year%2 == 0)); then
-            ubuntu_year=$((year-i))
-        else
-            ubuntu_year=$((year-i-1))
-        fi
-
+    if [ ! -f ./link/${tag}.txt ]; then
         touch ./link/${tag}.txt
+        version=$(echo ${tag} | sed -e "s/^\s*./V/g" | sed -e "s/-/_/g" | sed -e "s/\./_/g")
+        echo "Processing version: ${version}"
+        pushd CoppeliaSimLib && tag_date=$(git log -1 --format=%as coppeliasim-${tag}) && popd
+        year=$(date -d ${tag_date} +%y)
+        nums=(0 2 4)
+        for i in ${nums[@]}; do
+            if ((year%2 == 0)); then
+                ubuntu_year=$((year-i))
+            else
+                ubuntu_year=$((year-i-1))
+            fi
 
-        #echo $ubuntu_year
-        url="https://downloads.coppeliarobotics.com/${version}/CoppeliaSim_Edu_${version}_Ubuntu${ubuntu_year}_04.tar.xz"
-        wget --spider ${url}
-        if [ $? -eq 0 ]; then
-            echo "${version} ${url}" >> ./link/${tag}.txt
+            #echo $ubuntu_year
+            url="https://downloads.coppeliarobotics.com/${version}/CoppeliaSim_Edu_${version}_Ubuntu${ubuntu_year}_04.tar.xz"
+            wget --spider ${url}
+            if [ $? -eq 0 ]; then
+                echo "${version} ${url}" >> ./link/${tag}.txt
+            fi
+        done
+
+        if [ -s ./link/${tag}.txt ]; then
+            # The file is not-empty.
+            break
+        else
+            # The file is empty.
+            rm -f ./link/${tag}.txt
         fi
-    done
+    fi
 done
