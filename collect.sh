e0f17efbe11f704ef2d68bd9dbdaabddfea97a18
opensourcer2 <0x54e2c404c4C9Bf7722d4AC4f242F64C9be36fadd@hashmail.dev>
2024-09-15 12:47:16 -0700

    Update collect.sh

--- collect.sh
+++ collect.sh
@@ -11,11 +11,34 @@ popd
 tags=$(cat tag.txt)
 
 for tag in $tags; do
+    old_link=()
+
     if [ -f ./link/${tag}.txt ]; then
-        rm ./link/${tag}.txt
+        old_link_nossl=($(grep -oP "http://\K[^']+" ./link/${tag}.txt))
+        for link in "${old_link_nossl[@]}"; do
+            old_link+=("http://" + link)
+        done
+
+        old_link_ssl=($(grep -oP "https://\K[^']+" ./link/${tag}.txt))
+        for link in "${old_link_ssl[@]}"; do
+            old_link+=("https://" + link)
+        done
+
+        timestamp_line=$(head -n 1 ./link/${tag}.txt)
+        first_bracket=${timestamp_line:(1)}
+        last_bracket=${timestamp_line:(-1)}
+
+        timestamp="[$(date --utc +%FT%T.%3NZ)]"
+        if [ "${first_bracket}" = "[" ] && [ "${last_bracket}" = "]" ]; then
+            sed -i "1s/.*/$timestamp/" ./link/${tag}.txt
+        else
+            sed -i '1s/^/$timestamp\n/' ./link/${tag}.txt
+        fi
+    else
+        touch ./link/${tag}.txt
+        echo "[$(date --utc +%FT%T.%3NZ)]" > ./link/${tag}.txt
     fi
 
-    touch ./link/${tag}.txt
     version=$(echo ${tag} | sed -e "s/^\s*./V/g" | sed -e "s/-/_/g" | sed -e "s/\./_/g")
     echo "Processing version: ${version}"
     pushd CoppeliaSimLib && tag_date=$(git log -1 --format=%as coppeliasim-${tag}) && popd
@@ -32,22 +55,36 @@ for tag in $tags; do
         url="https://downloads.coppeliarobotics.com/${version}/CoppeliaSim_Edu_${version}_Ubuntu${ubuntu_year}_04.tar.xz"
         wget --spider ${url}
         if [ $? -eq 0 ]; then
-            echo "[$(date --utc +%FT%T.%3NZ)] ${version} ${url}" >> ./link/${tag}.txt
-            wget -P release ${url} 
+            for old_url in "${old_link[@]}"
+            do
+                if [ "$old_url" == "$url" ]; then
+                    echo "Already exist"
+                    break
+                else
+                    echo "${url}" >> ./link/${tag}.txt
+                    old_link+=($url)
+                fi
+            done
         fi
     done
 
+    for link in "${old_link[@]}"
+    do
+        wget --spider ${url}
+        if [ $? -eq 0 ]; then
+            wget -P release ${url}
+        fi
+    done
+             
     touch release_tag.txt
     echo "${tag}" > release_tag.txt
 
-    if [ -s ./link/${tag}.txt ]; then
-        # The file is not empty.
-        break
-    else
-        # The file is empty.
-        echo "Lost" > ./link/${tag}.txt
+    if [ ${#old_link[@]} -eq 0 ]; then
         touch release_link.txt
         echo "Lost" > release_link.txt
+        break
+    else
+        echo "Found " + "${#old_link[@]}" + " links"
         break
     fi
 done
