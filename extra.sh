5394607ba26b249522b33171bc30407283b1b7ac
opensourcer2 <0x54e2c404c4C9Bf7722d4AC4f242F64C9be36fadd@hashmail.dev>
2024-10-04 15:02:14 -0700

    Allow to download more releases

---
+++ extra.sh
@@ -0,0 +1,27 @@
+#! /bin/bash
+
+files=$(ls ./extra_link)
+
+for file in $files; do
+    if [ -s ./extra_link/${file} ]; then
+        #There are extra links to process
+        links=$(cat ./extra_link/${file})
+
+        for link in ${links[@]}; do
+            wget --spider ${link}
+            if [ $? -eq 0 ]; then
+                echo "Downloading ${link}"
+                wget -P release ${link} 
+            fi
+        done
+
+        extension="${file##*.}"
+        tag="${file%.*}"
+        touch release_tag.txt
+        echo "${tag}" > release_tag.txt
+
+        #Empty the file
+        true > ./extra_link/${file}
+        break
+    fi
+done
